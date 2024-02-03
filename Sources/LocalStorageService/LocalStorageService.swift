f2ef12fad144e6dd046e3dd7caa8a21cc1dc775a
Kyrylo Chernov <kyrylo.chernov@inventorsoft.co>
2024-02-03 12:09:23 +0200

    Added public

--- Sources/LocalStorageService/LocalStorageService.swift
+++ Sources/LocalStorageService/LocalStorageService.swift
@@ -2,7 +2,7 @@
 // https://docs.swift.org/swift-book
 import Foundation
 
-protocol LocalStorageService<TKey> {
+public protocol LocalStorageService<TKey> {
     associatedtype TKey: CustomStringConvertible
     func set(_ value: Any?, key: TKey)
     func get(key: TKey) -> Any?
@@ -13,38 +13,38 @@ protocol LocalStorageService<TKey> {
     func set<T>(key: TKey, value: T) where T : Codable
 }
 
-final class UserDefaultsService<TKey: CustomStringConvertible>: LocalStorageService {
-    typealias TKey = TKey
+public final class UserDefaultsService<TKey: CustomStringConvertible>: LocalStorageService {
+    public typealias TKey = TKey
     
     private let defaults = UserDefaults.standard
 
-    func set(_ value: Any?, key: TKey) {
+    public func set(_ value: Any?, key: TKey) {
         defaults.setValue(value, forKey: key.description)
     }
     
-    func set<T>(key: TKey, value: T) where T : Codable {
+    public func set<T>(key: TKey, value: T) where T : Codable {
         defaults.set(try? JSONEncoder().encode(value), forKey: key.description)
     }
 
-    func get(key: TKey) -> Any? {
+    public func get(key: TKey) -> Any? {
         return defaults.value(forKey: key.description)
     }
     
-    func get<T>(key: TKey) -> T? where T : Decodable {
+    public func get<T>(key: TKey) -> T? where T : Decodable {
         guard let data = defaults.object(forKey: key.description) as? Data else { return nil }
         return try? JSONDecoder().decode(T.self, from: data)
     }
     
-    func get<T>(key: TKey, defaultValue: T) -> T where T : Decodable {
+    public func get<T>(key: TKey, defaultValue: T) -> T where T : Decodable {
         guard let data = defaults.object(forKey: key.description) as? Data else { return defaultValue }
         return (try? JSONDecoder().decode(T.self, from: data)) ?? defaultValue
     }
 
-    func getBool(key: TKey) -> Bool {
+    public func getBool(key: TKey) -> Bool {
         return defaults.bool(forKey: key.description)
     }
 
-    func getString(key: TKey) -> String? {
+    public func getString(key: TKey) -> String? {
         return defaults.string(forKey: key.description)
     }
 }
